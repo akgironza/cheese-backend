dfe38a9a499259e21abaee885e10b2b494e9d7fa
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-08-09 18:03:04 -0400

    models, db connection, middleware

--- server.js
+++ server.js
@@ -4,14 +4,62 @@
 // get .env variables
 require("dotenv").config();
 
-// pull PORT from .env
-const { PORT = 4200 } = process.env;
+// get PORT, DATABASE_URL from .env
+const { PORT = 4200, DATABASE_URL} = process.env;
 
 // import express
 const express = require("express");
 
 // create express app object
 const app = express();
+
+// import mongoose
+const mongoose = require("mongoose");
+
+// import cors
+const cors = require("cors");
+
+// import morgan
+const morgan = require("morgan");
+
+
+////////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// establish connection
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+});
+
+// connection events
+mongoose.connection
+    .on("open", () => console.log("Connected to mongoose"))
+    .on("close", () => console.log("Disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const cheeseSchema = new mongoose.Schema({
+    name: String, 
+    image: String,
+    title: String
+});
+
+const Cheese = mongoose.model("Cheese", cheeseSchema);
+
+
+
+///////////////////////////////
+// MIDDLEWARE
+///////////////////////////////
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
 
 ///////////////////////////////
 // ROUTES
